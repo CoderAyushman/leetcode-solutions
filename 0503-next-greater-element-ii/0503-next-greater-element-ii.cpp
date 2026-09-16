d4ed0f18683902e549279a1208d97e9c2d059e83
Ayushman Mishra <113530294+CoderAyushman@users.noreply.github.com>
2026-09-16 20:49:38 +0530

    Time: 6 ms (40.36%), Space: 29.1 MB (11.87%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -4,14 +4,18 @@ public:
         int n = nums.size();
         stack<int> s;
         vector<int> res(n, -1);
-
-        for (int i = 2 * n - 1; i >= 0; --i) {
-            int num = nums[i % n];
-            while (!s.empty() && s.top() <= num)
+        for(int i=n-2;i>=0;i--){
+        s.push(nums[i]);
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            while(!s.empty()){
+                if(s.top()>nums[i]){
+                    res[i]=s.top();
+                    break;
+                }
                 s.pop();
-            if (!s.empty())
-                res[i % n] = s.top();
-            s.push(num);
+            }
+            s.push(nums[i]);
         }
         return res;
     }
